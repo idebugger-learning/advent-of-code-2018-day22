5448be1e8a9928538c5dda3f4fced5832df50ff9
iDeBugger Freeman <me@idebugger.io>
2023-07-23 19:58:25 +0200

    Part 2

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,12 @@ use crate::map::Map;
 mod map;
 
 fn main() {
+    // let mut map = Map::new(510, (10, 10));
     let mut map = Map::new(6084, (14, 709));
     let risk_level = map.get_risk_level((0, 0), (14, 709));
 
     println!("Risk level: {}", risk_level);
+
+    let distance = map.find_distance_to_target();
+    println!("Distance to target: {}", distance);
 }
--- src/map.rs
+++ src/map.rs
@@ -1,4 +1,9 @@
-use std::collections::HashMap;
+use std::collections::{HashMap};
+use petgraph::algo::dijkstra;
+use petgraph::prelude::*;
+
+const MOVE_COST: usize = 1;
+const CHANGE_GEAR_COST: usize = 7;
 
 #[derive(Copy, Clone)]
 pub enum RegionType {
@@ -7,11 +12,39 @@ pub enum RegionType {
     Narrow,
 }
 
+impl RegionType {
+    pub fn get_tools(&self) -> [Tool; 2] {
+        match self {
+            RegionType::Rocky => [Tool::ClimbingGear, Tool::Torch],
+            RegionType::Wet => [Tool::ClimbingGear, Tool::None],
+            RegionType::Narrow => [Tool::Torch, Tool::None],
+        }
+    }
+}
+
+#[derive(Copy, Clone, Hash, Eq, PartialEq)]
+pub enum Tool {
+    Torch,
+    ClimbingGear,
+    None,
+}
+
+impl Tool {
+    pub fn get_regions(&self) -> [RegionType; 2] {
+        match self {
+            Tool::ClimbingGear => [RegionType::Rocky, RegionType::Wet],
+            Tool::Torch => [RegionType::Rocky, RegionType::Narrow],
+            Tool::None => [RegionType::Wet, RegionType::Narrow],
+        }
+    }
+}
+
 pub struct Map {
     depth: usize,
     target: (usize, usize),
     regions: HashMap<(usize, usize), RegionType>,
     erosion_levels: HashMap<(usize, usize), usize>,
+    distances: HashMap<(usize, usize, Tool), usize>,
 }
 
 impl Map {
@@ -21,6 +54,7 @@ impl Map {
             target,
             regions: HashMap::new(),
             erosion_levels: HashMap::new(),
+            distances: HashMap::new(),
         }
     }
 
@@ -37,6 +71,60 @@ impl Map {
             }
         }
         risk_level
+    }
+
+    pub fn find_distance_to_target(&mut self) -> usize {
+        let graph = self.build_graph();
+
+        let zero_index = graph.node_indices().find(|n| graph[*n] == (0, 0, Tool::Torch)).unwrap();
+        let target_index = graph.node_indices().find(|n| graph[*n] == (self.target.0, self.target.1, Tool::Torch)).unwrap();
+        let distances = dijkstra(&graph, zero_index, Some(target_index), |e| *e.weight());
+        distances[&target_index]
+    }
+
+    fn build_graph(&mut self) -> UnGraph<(usize, usize, Tool), usize> {
+        let mut graph = UnGraph::new_undirected();
+        for x in 0..=(self.target.0 * 3) {
+            for y in 0..=(self.target.1 * 3) {
+                let region_type = self.get_region_type((x, y));
+                let tools = region_type.get_tools();
+                for tool in tools.iter() {
+                    graph.add_node((x, y, *tool));
+                }
+            }
+        }
+        for node in graph.node_indices() {
+            let coords = graph[node];
+            let current_region_type = self.get_region_type((coords.0, coords.1));
+            let current_tools = current_region_type.get_tools();
+            let neighbours = self.get_neighbours((coords.0, coords.1));
+            for neighbour in neighbours {
+                let region_type = self.get_region_type(neighbour);
+                let tools = region_type.get_tools();
+                for tool in tools.iter() {
+                    let neighbour = graph.node_indices().find(|n| graph[*n] == (neighbour.0, neighbour.1, *tool));
+                    if neighbour.is_none() {
+                        continue;
+                    }
+
+                    let neighbour = neighbour.unwrap();
+                    if tool == &coords.2 {
+                        graph.add_edge(node, neighbour, MOVE_COST);
+                    } else if current_tools.contains(tool) {
+                        graph.add_edge(node, neighbour, MOVE_COST + CHANGE_GEAR_COST);
+                    }
+                }
+            }
+        }
+
+        graph
+    }
+
+    fn get_neighbours(&mut self, coords: (usize, usize)) -> Vec<(usize, usize)> {
+        let mut neighbours = Vec::new();
+        neighbours.push((coords.0 + 1, coords.1));
+        neighbours.push((coords.0, coords.1 + 1));
+        neighbours
     }
 
     fn get_region_type(&mut self, coords: (usize, usize)) -> RegionType {
